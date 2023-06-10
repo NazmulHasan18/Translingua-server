2752fce66d291f49db641596cfc57dd01b00baf8
Nazmul Hasan18 <nazmul182218@gmail.com>
2023-06-10 20:01:34 +0600

    popular classes and classes api get

--- index.js
+++ index.js
@@ -28,23 +28,30 @@ async function run() {
    try {
       // Connect the client to the server	(optional starting in v4.7)
       await client.connect();
-      // !here is starting all operations
 
-      const instructorCollection = client.db("translinguaDB").collection("instructors");
+      // ?here is starting all operations
+
       const quoteCollection = client.db("translinguaDB").collection("quotes");
+      const instructorCollection = client.db("translinguaDB").collection("instructors");
+      const classCollection = client.db("translinguaDB").collection("classes");
 
       // !jwt token create and post
+
       app.post("/jwt", (req, res) => {
          const user = req.body;
          const token = jwt.sign({ user }, process.env.PRIVATE_KEY, { expiresIn: "1h" });
          res.send(token);
       });
 
+      // ?its quotes api
+
       app.get("/quotes", async (req, res) => {
          const quotes = await quoteCollection.find({}).toArray();
          const result = quotes[parseInt(Math.round(Math.random() * 100))];
          res.send(result);
       });
+
+      // ? there are instructor apis
 
       app.get("/instructors", async (req, res) => {
          const result = await instructorCollection.find({}).sort({ current_students: -1 }).toArray();
@@ -54,6 +61,22 @@ async function run() {
       app.get("/instructor/:id", async (req, res) => {
          const id = req.params.id;
          const result = await instructorCollection.findOne({ _id: new ObjectId(id) });
+         res.send(result);
+      });
+
+      // ?Here popular classes apis
+
+      app.get("/popular_classes", async (req, res) => {
+         const result = await classCollection
+            .find({ status: "approved" })
+            .sort({ current_students: -1 })
+            .toArray();
+
+         res.send(result.slice(0, 6));
+      });
+
+      app.get("/classes", async (req, res) => {
+         const result = await classCollection.find({}).toArray();
          res.send(result);
       });
 
