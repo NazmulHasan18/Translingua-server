2681ed2ebe53db05bc04540a667ec5be1ab9be30
NazmulHasan18 <nazmul182218@gmail.com>
2024-09-13 07:38:20 +0600

    redeploy

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ const jwtVerify = (req, res, next) => {
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lvw8wzq.mongodb.net/?retryWrites=true&w=majority`;
-
+console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
    serverApi: {
